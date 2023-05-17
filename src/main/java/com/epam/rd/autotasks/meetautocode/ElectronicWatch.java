c797ce4032d987127ae3cf3b34f975483b36494f
tornike khokhiashvili <tornikexoxiashvili@gmail.com>
2023-05-17 18:07:16 +0000

    Replace ElectronicWatch.java

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -7,6 +7,11 @@ public class ElectronicWatch {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
 
+        int hours = (seconds / 3600)%24;
+        int minutes = (seconds / 60) % 60;
+        int remainingSeconds = seconds % 60;
 
+        String formattedTime = String.format("%d:%02d:%02d", hours, minutes, remainingSeconds);
+        System.out.println(formattedTime);
     }
 }
